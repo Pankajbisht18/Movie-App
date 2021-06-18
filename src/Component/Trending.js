581f74535bbfe6df33e0f15b8c63ca95743c3f8a
Pankaj <techpankaj45@gmail.com>
2021-06-18 22:52:33 +0530

    Add trending page content and make it responsive

--- src/Component/Trending.js
+++ src/Component/Trending.js
@@ -1,8 +1,30 @@
+import { useState, useEffect } from "react";
+import './Trending.css';
+
 const Trending = () => {
+    const [trend, setTrend] = useState([]);
+    useEffect(()=>{
+        fetch("https://api.themoviedb.org/3/trending/all/day?api_key=a619788e83ab1a8966b53d0814fc73d9")
+        .then(res=>res.json())
+        .then(json => setTrend(json.results));
+    },[])
+    console.log(trend)
     return(
-        <>
-            <h1>Trending</h1>
-        </>
+        <div>
+            <h1 className="heading">Trending</h1>
+            <div className="trending">
+                {trend.map((movie,id)=>{
+                    return(
+                        <div className="card" key={movie.id}>
+                            <img src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt="Image not available" />
+                            <p className="movieTitle">{movie.name || movie.original_title}</p>
+                            <p className="movieVote">Rating: {movie.vote_average}</p>
+
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
     );
 }
 export default Trending;
