8b252d4ecc5225ea7ffa213775fb53959079804b
Pankaj <techpankaj45@gmail.com>
2021-07-07 23:17:36 +0530

    Modify navbar

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,29 +1,49 @@
 import { Link } from 'react-router-dom';
-import './Navbar.css';
-
+import './Navbar.css'
 const Navbar = () => {
+    const resNav = () => {
+        const hamburger = document.querySelector(".hamburger");
+        const navMenu = document.querySelector(".nav-menu1");
+        function mobileMenu() {
+            hamburger.classList.toggle("active");
+            navMenu.classList.toggle("active");
+        }
+        mobileMenu();
+    }
+    const closeRes = () => {
+        const hamburger = document.querySelector(".hamburger");
+        const navMenu = document.querySelector(".nav-menu1");
+        function closeMenu() {
+            hamburger.classList.remove("active");
+            navMenu.classList.remove("active");
+        }
+        closeMenu();
+    }
     return(
-        <nav className="menu">
-            <input type="checkbox" id="check" />
-            <label for="check" className="checkbtn">
-                <i className="fas fa-bars"></i>
-            </label>
-            <label className="logo">Movie App</label>
-            <ul>
-                <li>
-                    <Link to="/">Trending</Link>
-                </li>
-                <li>
-                    <Link to="/movie">Movie</Link>
-                </li>
-                <li>
-                    <Link to="/tvshow">Tv Show</Link>
-                </li>
-                <li>
-                    <Link to="/search">Search</Link>
-                </li>
-            </ul>
-        </nav>
+        <header className="header">
+            <nav className="navbar1">
+                <h3 className="nav-logo1">Movie App</h3>
+                <ul className="nav-menu1">
+                    <li className="nav-item1">
+                        <Link to="/" className="nav-link1" onClick={closeRes}>Trending</Link>
+                    </li>
+                    <li className="nav-item1">
+                        <Link to="/movie" className="nav-link1" onClick={closeRes}>Movie</Link>
+                    </li>
+                    <li className="nav-item1">
+                        <Link to="/tvshow" className="nav-link1" onClick={closeRes}>Tv Show</Link>
+                    </li>
+                    <li className="nav-item1">
+                        <Link to="/search" className="nav-link1" onClick={closeRes}>Search</Link>
+                    </li>
+                </ul>
+                <div className="hamburger" onClick={resNav}>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </div>
+            </nav>
+        </header>
     );
 }
 export default Navbar;
